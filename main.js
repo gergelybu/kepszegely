865c28d5164313e1a1c01c63b84715602ed44917
gergelybu <gergelyburai@gmail.com>
2023-03-08 11:30:48 +0100

    Feladat elkészítése

---
+++ main.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", init);
+
+function init() {
+  const Lista = [];
+  const kepek = document.querySelectorAll("article img");
+  for (let index = 0; index < kepek.length; index++) {
+    let text = kepek[index].src;
+    word = text.split("http://127.0.0.1:5500/");
+    Lista.push(word[1]);
+    //Lista.push(kepek[index].src);
+  }
+  console.log(Lista);
+  for (let index = 0; index < kepek.length; index++) {
+    kepek[index].addEventListener("mouseover", szegelyvaltas);
+    kepek[index].addEventListener("mouseout", szegelyle);
+  }
+}
+
+function szegelyvaltas(event) {
+  const ELEM = event.target.parentNode;
+  console.log(ELEM);
+  ELEM.classList.add("szegely");
+}
+
+function szegelyle(event) {
+  const ELEM = event.target.parentNode;
+  console.log(ELEM);
+  ELEM.classList.remove("szegely");
+}
